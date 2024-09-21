fe2d2bf192ca5de1fb30d3db17144ee5a5f4abb7
DDJ-02 <149932599+DDJ-02@users.noreply.github.com>
2024-09-21 18:55:46 +0530

    password successful page added

--- app/src/main/java/com/example/madgroupgit/PasswordUpdatedSuccessPage.kt
+++ app/src/main/java/com/example/madgroupgit/PasswordUpdatedSuccessPage.kt
@@ -8,11 +8,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class PasswordResetPage : AppCompatActivity() {
+class PasswordUpdatedSuccessPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_password_reset_page)
+        setContentView(R.layout.activity_password_updated_success_page)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -20,8 +20,8 @@ class PasswordResetPage : AppCompatActivity() {
         }
     }
 
-    fun goToPasswordUpdatedPage(view: View) {
-        val passwordSuccesPage = Intent(this,PasswordUpdatedSuccessPage::class.java)
-        startActivity(passwordSuccesPage)
+    fun goToLoginPage(view: View) {
+        val login = Intent(this,Login::class.java)
+        startActivity(login)
     }
 }
