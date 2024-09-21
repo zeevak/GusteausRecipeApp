a604405c6bc603180d6b783bc7ba8d99d7161f25
DDJ-02 <149932599+DDJ-02@users.noreply.github.com>
2024-09-21 18:30:21 +0530

    email verification structure done

--- app/src/main/java/com/example/madgroupgit/EmailVerificationPage.kt
+++ app/src/main/java/com/example/madgroupgit/EmailVerificationPage.kt
@@ -1,6 +1,8 @@
 package com.example.madgroupgit
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,10 @@ class EmailVerificationPage : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    fun goToSetNewPasswordPage(view: View) {
+        val setNewPasswordPage = Intent(this,PasswordResetPage::class.java);
+        startActivity(setNewPasswordPage)
     }
 }
--- app/src/main/java/com/example/madgroupgit/PasswordResetPage.kt
+++ app/src/main/java/com/example/madgroupgit/PasswordResetPage.kt
@@ -1,27 +1,20 @@
 package com.example.madgroupgit
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class Login : AppCompatActivity() {
+class PasswordResetPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_password_reset_page)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-    }
-
-    fun goToForgotPassword(view: View) {
-        val forgetPasswordPage = Intent(this,ForgotPassword::class.java)
-        startActivity(forgetPasswordPage)
     }
 }
